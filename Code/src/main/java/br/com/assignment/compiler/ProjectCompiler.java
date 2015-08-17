b9622daeba318522fb4ae6f2f959ca4d95b3a300
Cindy Soares <cindy.as@gmail.com>
2015-08-17 12:19:35 -0300

    retornando log ao abrir arquivo zip

--- Code/src/main/java/br/com/assignment/compiler/ProjectCompiler.java
+++ Code/src/main/java/br/com/assignment/compiler/ProjectCompiler.java
@@ -1,7 +1,9 @@
 package br.com.assignment.compiler;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -10,33 +12,45 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+import org.apache.log4j.WriterAppender;
+
 @Path("/compiler")
 public class ProjectCompiler {
+	
+	private static final Logger log = Logger.getLogger(ProjectCompiler.class);
 
 	@POST
     @Path("/run")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public String run(InputStream javaProject) {
-		StringBuilder log = new StringBuilder();
-		log.append("Opening zip file ... " );
+		log.setLevel(Level.INFO);
+		SimpleLayout layout = new SimpleLayout();
+		OutputStream logOutputStream = new ByteArrayOutputStream();
+		WriterAppender appender = new WriterAppender(layout, logOutputStream);
+		log.addAppender(appender);
+		
+		log.info("Opening zip file ... " );
 		ZipInputStream zipInputStream = null;
 		try {
 			zipInputStream = new ZipInputStream(javaProject);
 			ZipEntry zipEntry;
 			while((zipEntry = zipInputStream.getNextEntry())!= null) {
-				log.append("\nExtracting " + zipEntry.getName());
+				log.info("Extracting " + zipEntry.getName());
 				byte [] buf = new byte[(int) zipEntry.getSize()];
 	            zipInputStream.read(buf);
 			}
 		} catch (IOException e) {
-			log.append(e.getMessage());
+			log.error(e.getMessage(), e);
 		} finally {
 			try {
 				zipInputStream.close();
 			} catch (IOException ignored) {
 			}
 		}
-		return log.toString();
+		return logOutputStream.toString();
 	}
 	
 }
--- Code/src/test/java/br/com/assignment/compiler/ProjectCompilerTest.java
+++ Code/src/test/java/br/com/assignment/compiler/ProjectCompilerTest.java
@@ -9,18 +9,35 @@ import org.junit.Test;
 
 public class ProjectCompilerTest {
 	
+	@Test
 	public void testCorruptedZip() throws Exception {
 		InputStream request = new FileInputStream(
 				new File(this.getClass().getResource("./HelloWorld-corrupted.zip").toURI()));
 		String response = new ProjectCompiler().run(request);
-		Assert.assertEquals("", response);
+		Assert.assertEquals(
+				"INFO - Opening zip file ... \r\n"+
+				"INFO - Extracting HelloWorld.java\r\n", response);
 	}
 	
 	@Test
 	public void testMavenProjectZip() throws Exception {
 		InputStream request = new FileInputStream(
 				new File(this.getClass().getResource("./HelloWorld-maven-project.zip").toURI()));
-		new ProjectCompiler().run(request);
+		String response = new ProjectCompiler().run(request);
+		Assert.assertEquals(
+				"INFO - Opening zip file ... \r\n" +
+				"INFO - Extracting pom.xml\r\n" +
+				"INFO - Extracting src/\r\n" +
+				"INFO - Extracting src/main/\r\n" +
+				"INFO - Extracting src/main/java/\r\n" +
+				"INFO - Extracting src/main/java/helloworld/\r\n" +
+				"INFO - Extracting src/main/java/helloworld/HelloWorld.java\r\n" +
+				"INFO - Extracting src/main/resources/\r\n" +
+				"INFO - Extracting src/test/\r\n" +
+				"INFO - Extracting src/test/java/\r\n" +
+				"INFO - Extracting src/test/java/helloworld/\r\n" +
+				"INFO - Extracting src/test/java/helloworld/HelloWorldTest.java\r\n" +
+				"INFO - Extracting src/test/resources/\r\n", response);
 	}
 
 }
